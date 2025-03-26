720667d5e47471af2e2f8e25a35f91c256566e7c
root <39844782+NE137@users.noreply.github.com>
2025-03-26 22:10:40 +0100

    fix notation

--- src/commands/economy/bal.js
+++ src/commands/economy/bal.js
@@ -7,6 +7,6 @@ module.exports = {
   execute: async inter => {
     let d = await inter.client.datadb.getusr(inter.user.id, "debt");
       await inter.reply({ content: `You currently have \`${await inter.client.datadb.getusr(inter.user.id, "bal")} ${inter.client.currency}.\`` +
-       (d ? `\nYou have an outstanding debt of \`${inter.client.currency}${d}\`` : "Want more money? You can borrow money from Jack using \`/loan borrow\`"), ephemeral: true });
+       (d ? `\nYou have an outstanding debt of \`${inter.client.currency}${d}\`` : `Want more money? You can borrow money from Jack using \`/loan borrow\``), ephemeral: true });
   }
 }
