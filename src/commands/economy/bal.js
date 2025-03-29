88de55639f4f9ffccf858b9763c05346f25136aa
root <39844782+NE137@users.noreply.github.com>
2025-03-29 08:09:57 +0100

    fix stupid typo

--- src/commands/economy/bal.js
+++ src/commands/economy/bal.js
@@ -18,7 +18,7 @@ module.exports = {
         await inter.reply({ content: `You currently have \`${inter.client.currency}${await inter.client.datadb.getusr(inter.user.id, "bal")}\`.` +
          (d ? `\nYou have an outstanding debt of \`${inter.client.currency}${d}\`` : `\nWant more money? You can borrow money from Jack using \`/loan borrow\``), ephemeral: true });
       } else {
-        let d = await account.client.datadb.getusr(account.user.id, "debt");
+        let d = await inter.client.datadb.getusr(account.user.id, "debt");
         await inter.reply({ content: `${account.username} currently has \`${inter.client.currency}${await inter.client.datadb.getusr(account.id, "bal")}\`.` +
          (d ? `\nThey have an outstanding debt of \`${inter.client.currency}${d}\`` : ``), ephemeral: true });
       }
