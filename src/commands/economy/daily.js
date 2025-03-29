ffdaafb8d219485050986e861d444c8f3e83c01c
root <39844782+NE137@users.noreply.github.com>
2025-03-29 18:20:56 +0100

    daily multiplier

--- src/commands/economy/daily.js
+++ src/commands/economy/daily.js
@@ -5,6 +5,8 @@ module.exports = {
     .setName("daily")
     .setDescription("Claim your daily money amuont."),
   execute: async inter => {
+
+
     let candaily = await inter.client.datadb.sql`
       select last_daily from users
       where
@@ -13,24 +15,36 @@ module.exports = {
         snowflake = ${inter.user.id} and last_daily is null`;
 
     if (candaily.length) { // if anything was returned
+
+
+      const dailyBooster = "1246226561959464980";
+      const dailyContributor = "1288518414121304138";
+      let multiplier = 1;
+
+      if (inter.member.roles.cache.has(dailyBooster)) multiplier = 1.5;
+      if (inter.member.roles.cache.has(dailyContributor)) multiplier = 2;
+      
+
+      let dailyTotal = inter.client.xconfig.daily * multiplier;
+
       await inter.client.datadb.sql`update users set last_daily = 'today' where snowflake = ${inter.user.id}`;
 
       let d = await inter.client.datadb.getusr(inter.user.id, "debt");
-      if (d >= inter.client.xconfig.daily) {
-        await inter.client.datadb.modusr(inter.user.id, "debt", -inter.client.xconfig.daily);
-        await inter.client.datadb.modconf(inter.guildId, "loanpool", inter.client.xconfig.daily);
+      if (d >= dailyTotal) {
+        await inter.client.datadb.modusr(inter.user.id, "debt", -dailyTotal*multiplier);
+        await inter.client.datadb.modconf(inter.guildId, "loanpool", dailyTotal*multiplier);
 
-        await inter.reply(`Claimed daily of \`${inter.client.currency}${inter.client.xconfig.daily}\`.\nDue to your outstanding debts, this has all been automatically redirected into paying them off.`);
+        await inter.reply(`Claimed daily of \`${inter.client.currency}${dailyTotal}\`.\nDue to your outstanding debts, this has all been automatically redirected into paying them off.`);
       } else if (d > 0) {
-        let left = inter.client.xconfig.daily - d;
+        let left = dailyTotal - d;
 
         await inter.client.datadb.modusr(inter.user.id, "debt", -d);
         await inter.client.datadb.modusr(inter.user.id, "bal", left);
 
-        await inter.reply(`Claimed daily of \`${inter.client.currency}${inter.client.xconfig.daily}\`.\n\`${d} ${inter.client.currency}\` of this has been deducted to finish paying off your debts.`);
+        await inter.reply(`Claimed daily of \`${inter.client.currency}${dailyTotal}\`.\n\`${d} ${inter.client.currency}\` of this has been deducted to finish paying off your debts.`);
       } else {
-        await inter.client.datadb.modusr(inter.user.id, "bal", inter.client.xconfig.daily);
-        await inter.reply(`Successfully claimed daily of \`${inter.client.currency}${inter.client.xconfig.daily}\`!`);
+        await inter.client.datadb.modusr(inter.user.id, "bal", dailyTotal);
+        await inter.reply(`Successfully claimed daily of \`${inter.client.currency}${dailyTotal}\`!`);
       }
     } else {
       s_until = ((Date.now()/86400_000>>0)+1)*86400;
