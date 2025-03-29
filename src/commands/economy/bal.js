f9beadd84176a060e9c6cb98a8b907ea3cc6fbe2
root <39844782+NE137@users.noreply.github.com>
2025-03-29 07:52:42 +0100

    changed balance command to allow to see balance of others

--- src/commands/economy/bal.js
+++ src/commands/economy/bal.js
@@ -3,10 +3,23 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("balance")
-    .setDescription("Shows how much money you have."),
+    .setDescription("Shows how much money you have.")
+    .addUserOption(opt =>
+      opt.setName("account")
+            .setDescription("Whose balance do you wish to see?")
+            .setRequired(false)),
   execute: async inter => {
-    let d = await inter.client.datadb.getusr(inter.user.id, "debt");
+
+    let account = inter.options.getUser("account")
+    if (inter.user.id == account.id) {
+      let d = await inter.client.datadb.getusr(inter.user.id, "debt");
       await inter.reply({ content: `You currently have \`${inter.client.currency}${await inter.client.datadb.getusr(inter.user.id, "bal")}\`.` +
        (d ? `\nYou have an outstanding debt of \`${inter.client.currency}${d}\`` : `\nWant more money? You can borrow money from Jack using \`/loan borrow\``), ephemeral: true });
+    } else {
+      let d = await account.client.datadb.getusr(account.user.id, "debt");
+      await inter.reply({ content: `${account.username} currently has \`${inter.client.currency}${await inter.client.datadb.getusr(account, "bal")}\`.` +
+       (d ? `\nThey have an outstanding debt of \`${inter.client.currency}${d}\`` : ``), ephemeral: true });
+    }
+   
   }
 }
