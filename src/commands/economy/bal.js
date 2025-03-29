dbaa899906147d5fdc3f1e8415ec7c128183f63f
root <39844782+NE137@users.noreply.github.com>
2025-03-29 08:00:02 +0100

    fix balance

--- src/commands/economy/bal.js
+++ src/commands/economy/bal.js
@@ -10,15 +10,15 @@ module.exports = {
             .setRequired(false)),
   execute: async inter => {
 
-    let account;
-    account = inter.options.getUser("account")
+    let account = '';
+    account = inter.options.getUser("account");
     if (inter.user.id == account.id) {
       let d = await inter.client.datadb.getusr(inter.user.id, "debt");
       await inter.reply({ content: `You currently have \`${inter.client.currency}${await inter.client.datadb.getusr(inter.user.id, "bal")}\`.` +
        (d ? `\nYou have an outstanding debt of \`${inter.client.currency}${d}\`` : `\nWant more money? You can borrow money from Jack using \`/loan borrow\``), ephemeral: true });
     } else {
       let d = await account.client.datadb.getusr(account.user.id, "debt");
-      await inter.reply({ content: `${account.username} currently has \`${inter.client.currency}${await inter.client.datadb.getusr(account, "bal")}\`.` +
+      await inter.reply({ content: `${account.username} currently has \`${inter.client.currency}${await inter.client.datadb.getusr(account.id, "bal")}\`.` +
        (d ? `\nThey have an outstanding debt of \`${inter.client.currency}${d}\`` : ``), ephemeral: true });
     }
    
