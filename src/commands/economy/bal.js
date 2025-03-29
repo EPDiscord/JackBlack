7e67d4296587cfc06da39881057c56e49cf8e3a5
root <39844782+NE137@users.noreply.github.com>
2025-03-29 08:08:50 +0100

    add try catch to balance

--- src/commands/economy/bal.js
+++ src/commands/economy/bal.js
@@ -7,20 +7,25 @@ module.exports = {
     .addUserOption(opt =>
       opt.setName("account")
             .setDescription("Whose balance do you wish to see?")
-            .setRequired(false)),
+            .setRequired(true)),
   execute: async inter => {
 
-    let account = '';
+    let account = 0;
     account = inter.options.getUser("account");
-    if (inter.user.id == account.id) {
-      let d = await inter.client.datadb.getusr(inter.user.id, "debt");
-      await inter.reply({ content: `You currently have \`${inter.client.currency}${await inter.client.datadb.getusr(inter.user.id, "bal")}\`.` +
-       (d ? `\nYou have an outstanding debt of \`${inter.client.currency}${d}\`` : `\nWant more money? You can borrow money from Jack using \`/loan borrow\``), ephemeral: true });
-    } else {
-      let d = await account.client.datadb.getusr(account.user.id, "debt");
-      await inter.reply({ content: `${account.username} currently has \`${inter.client.currency}${await inter.client.datadb.getusr(account.id, "bal")}\`.` +
-       (d ? `\nThey have an outstanding debt of \`${inter.client.currency}${d}\`` : ``), ephemeral: true });
+    try {
+      if (inter.user.id == account.id) {
+        let d = await inter.client.datadb.getusr(inter.user.id, "debt");
+        await inter.reply({ content: `You currently have \`${inter.client.currency}${await inter.client.datadb.getusr(inter.user.id, "bal")}\`.` +
+         (d ? `\nYou have an outstanding debt of \`${inter.client.currency}${d}\`` : `\nWant more money? You can borrow money from Jack using \`/loan borrow\``), ephemeral: true });
+      } else {
+        let d = await account.client.datadb.getusr(account.user.id, "debt");
+        await inter.reply({ content: `${account.username} currently has \`${inter.client.currency}${await inter.client.datadb.getusr(account.id, "bal")}\`.` +
+         (d ? `\nThey have an outstanding debt of \`${inter.client.currency}${d}\`` : ``), ephemeral: true });
+      }
+    } catch (exception) {
+      await inter.reply({ content: `That user has never played a game.`, ephemeral: true })
     }
+    
    
   }
 }
